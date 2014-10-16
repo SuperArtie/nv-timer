fc42363f2760ceda5e78fd5c9e7bb70abc895715
Nathan Vasquez <nathan.superartie@gmail.com>
2014-10-16 10:01:53 -0500

    last commit

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -1,7 +1,21 @@
 (function(){
   'use strict';
-  angular.module('nv-calculator')
-  .controller('MainCtrl', ['$scope', function($scope){
+  angular.module('nv-timer')
+  .controller('MainCtrl', ['$scope', '$interval', '$timeout', function($scope, $interval, $timeout){
     $scope.title = 'Heavy D n da Boyz';
+    $scope.sec     = 0;
+    $scope.min     = 0;
+    $scope.startTimer = function(num){
+      $scope.unitSec = num*60;
+      var go = $interval(function(){
+        $scope.unitSec--;
+        $scope.min = Math.floor($scope.unitSec/60);
+        $scope.sec = $scope.unitSec-60*$scope.min;
+      }, 1000);
+      $timeout(function(){
+        $interval.cancel(go);
+        navigator.vibrate(5000);
+        }, num*60000);
+    };
   }]);
 })();
